2994e386d78736c470bf273d4e6213fbe81fa205
Rich Edwards <richedw@gmail.com>
2013-12-12 11:40:18 -0600

    Add validation to ensure a movie title is present.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ActiveRecord::Base
   has_many :showtimes
   has_many :roles
   has_many :stars, through: :roles
+  validates :title, presence: true
 
   def rotten_finder
     RottenMovie.find(title: title, limit: 1)
