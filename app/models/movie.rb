76e37d65b2cb0a605cd975ee37e9ca52de2938a1
Rich Edwards <richedw@gmail.com>
2013-12-12 11:22:52 -0600

    Add failing test for audience_rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,6 +8,10 @@ class Movie < ActiveRecord::Base
   end
   
   def snippet
-    description.truncate 50
+    description.to_s.truncate 50
+  end
+
+  def audience_rating
+
   end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Movie do
   let(:movie) {Movie.new}
+  let(:rotten_finder_return) {OpenStruct.new(ratings: Openstruct.new(audience_score: 84))}
 
   describe "#snippet" do
     context "when description is less than or equal to 50 characters" do
@@ -17,6 +18,24 @@ describe Movie do
         allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge.")
         
         expect(movie.snippet).to eq("When a Roman general is betrayed and his family...")
+      end
+    end
+
+    context "when description is empty" do
+      it "returns nil" do
+        allow(movie).to receive(:description).and_return()
+
+        expect(movie.snippet).to eq("")
+      end
+    end
+  end
+
+  describe "#audience_rating" do
+    context "when movie is found on Rotten Tomatoes" do
+      it "returns the audience score for the movie" do
+        allow(movie).to receive(:rotten_finder).and_return(rotten_finder_return)
+
+        expect(movie.audience_rating).to eq(84)
       end
     end
   end
